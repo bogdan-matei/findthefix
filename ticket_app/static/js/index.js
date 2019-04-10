7397732247eef923624e1f0d4e302431acf4d309
riordachioaia <i.radu@yahoo.com>
2019-04-10 11:41:40 +0300

    first commit - fixed locally

--- ticket_app/static/js/index.js
+++ ticket_app/static/js/index.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-   $('form > div:nth-child(4) > div > input').focusout(function(){$('form > div:nth-child(4) > div > input').val("")});
-   $('form > div:nth-child(5)').hide();
+   /*$('form > div:nth-child(4) > div > input').focusout(function(){$('form > div:nth-child(4) > div > input').val("")});*/
+   /*$('form > div:nth-child(5)').hide();*/
 });
